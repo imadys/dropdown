9920bec6f237e1d9d3a132b68044fbbbe30cee9f
imadys <imadys@outlook.com>
2023-02-16 20:16:09 +0300

    config update

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, "lib/src/index.js"),
-            name: "IMADYS",
+            name: "dropdown",
             formats: ["es", "umd"],
-            fileName: (format) => `imadys.${format}.js`
+            fileName: (format) => `dropdown.${format}.js`
         }
     }
 })
